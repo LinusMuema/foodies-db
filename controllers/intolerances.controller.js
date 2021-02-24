561f568367b04ffe72f7b16b49b9b5e6392f3d21
linus muema <linus.m.muema@gmail.com>
2021-02-24 13:53:05 +0300

    - update intolerances

--- controllers/intolerances.controller.js
+++ controllers/intolerances.controller.js
@@ -11,7 +11,7 @@ exports.getAllIntolerances = async (req, res) => {
 
 exports.updateIntolerances = async (req, res) => {
     try {
-        const update = await User.findOneAndUpdate({email: req.email}, {intolerances: req.body.intolerances}, {new: true})
-        res.status(200).json(update)
+        await User.findOneAndUpdate({email: req.email}, {intolerances: req.body.intolerances})
+        res.status(200).json({status: 'success'})
     } catch (e){ response.serverError(res, e.message) }
 }
